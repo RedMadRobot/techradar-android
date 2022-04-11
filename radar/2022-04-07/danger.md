86eca741da7c4bef2d8bb0a8793bb436e4858c19
Andrey Tolmachev <a.tolmachev@redmadrobot.com>
2022-04-11 21:19:59 +0300

    Add prebuild script: edit and discussion links

---
+++ radar/2022-04-07/danger.md
@@ -0,0 +1,8 @@
+---
+title:      Danger
+ring:       adopt
+quadrant:   utilities
+info:       Stop saying "you forgot to …" in code review
+
+---
+
---
+++ script/addEditLinks.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import os
+import glob
+
+def findLatestFiles():
+    files = glob.glob("radar/*/*.md")
+    latestFiles = dict()
+
+    for filePath in files:
+        fileName = filePath.split("/")[-1]
+        latestFiles[fileName] = filePath
+
+    return list(latestFiles.values())
+
+def appendToFiles(files):
+    link = "https://github.com/RedMadRobot/techradar/edit/main/"
+
+    for filePath in files:
+        with open(filePath, "a") as file:
+            appendText = "\n" + "Links: [edit page]({}), [open discussion](https://github.com/RedMadRobot/techradar/discussions/new)".format(link + filePath) + "\n"
+            file.write(appendText)
+
+def main():
+    contentFiles = findLatestFiles()
+    appendToFiles(contentFiles)
+
+
+if __name__ == "__main__":
+    main()
